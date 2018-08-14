7127141e249fbe599470fe7d23d24945bd21d349
OttoMoses <ottomoses@gmail.com>
2018-08-14 07:06:59 -0700

    fix damage handler for threats untargetable at range three

--- app/src/main/java/com/otto/spacealertresolver/ThreatActions/External/OnDamageExternalExcludeRange.java
+++ app/src/main/java/com/otto/spacealertresolver/ThreatActions/External/OnDamageExternalExcludeRange.java
@@ -3,22 +3,51 @@ package com.otto.spacealertresolver.ThreatActions.External;
 import android.util.Pair;
 
 import com.otto.spacealertresolver.ExternalDamageBundle;
+import com.otto.spacealertresolver.Threats.Threat;
 import com.otto.spacealertresolver.Threats.ThreatExternal;
 
-/**
- * Created by Otto on 3/6/2018.
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import static com.otto.spacealertresolver.Activities.MainActivity.game;
 
 public class OnDamageExternalExcludeRange extends OnDamageExternal
 {
-
-
     @Override
-    public String Execute(ThreatExternal threat, ExternalDamageBundle db)
+    public String Execute(ThreatExternal t, ExternalDamageBundle db)
     {
+        String message = "";
         int DBDamage = 0;
         int damage;
-        if(!threat.rangeThree)
+        ThreatExternal target = null;
+        ExternalDamageBundle bypass = null;
+        if(t.rangeThree)
+        {
+            message += "The " + t.name + " is too far away to target!";
+            ArrayList<Threat> targets = new ArrayList<>();
+            for(Threat threat : game.activeThreats)
+            {
+                if(!threat.name.equals(t.name) && threat.track == t.track)
+                {
+                    targets.add(threat);
+                }
+                if(targets.size() != 0)
+                {
+
+                    Collections.sort(targets, new Comparator<Threat>() {
+                        @Override
+                        public int compare(Threat threat, Threat t1) {
+                            return threat.compareTo(t1);
+                        }
+                    });
+                    target = (ThreatExternal) threat;
+                    target.bundle = db;
+                }
+            }
+
+        }
+        else
         {
             for(Pair p : db.damageSources)
             {
@@ -30,21 +59,18 @@ public class OnDamageExternalExcludeRange extends OnDamageExternal
             }
             else
             {
-                damage = DBDamage - threat.shield;
-                if (damage > 0)
+                damage = DBDamage - t.shield;
+                if(damage > 0)
                 {
-                    threat.damage += damage;
-                    return "\nThe " + threat.name + " takes " + damage + " damage!\n";
+                    t.damage += damage;
+                    message += "\nThe " + t.name + " takes " + damage + " damage!\n";
                 }
                 else
                 {
-                    return "\nThe " + threat.name + " blocks all damage!";
+                    message += "\nThe " + t.name + " blocks all damage!";
                 }
             }
         }
-        else
-        {
-            return "\nThe " + threat.name + " is too far away to target!";
-        }
+        return message;
     }
 }
