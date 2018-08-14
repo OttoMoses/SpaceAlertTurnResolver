e210b3ad742584189b8ad52d082e8c4dabbd4a40
OttoMoses <ottomoses@gmail.com>
2018-08-14 05:51:20 -0700

    fix unrevisioned file

---
+++ app/src/main/java/com/otto/spacealertresolver/ExternalDamageBundle.java
@@ -0,0 +1,16 @@
+package com.otto.spacealertresolver;
+
+import android.util.Pair;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Otto on 3/5/2018.
+ */
+
+public class ExternalDamageBundle {
+    public ArrayList<Pair> damageSources;
+    public ExternalDamageBundle() {
+        this.damageSources = new ArrayList<>();
+    }
+}
