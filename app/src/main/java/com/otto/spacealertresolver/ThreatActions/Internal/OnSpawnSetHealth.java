ec3d17b6445b99f15a9f7a9a7ab6a2a1aa5263fb
OttoMoses <ottomoses@gmail.com>
2018-08-18 06:16:33 -0700

    added code to instantly kill unstable warheads threat if it spawns with 0 health

--- app/src/main/java/com/otto/spacealertresolver/ThreatActions/Internal/OnSpawnSetHealth.java
+++ app/src/main/java/com/otto/spacealertresolver/ThreatActions/Internal/OnSpawnSetHealth.java
@@ -4,11 +4,17 @@ import com.otto.spacealertresolver.Activities.MainActivity;
 import com.otto.spacealertresolver.Section;
 import com.otto.spacealertresolver.Threats.ThreatInternal;
 
+import static com.otto.spacealertresolver.Activities.MainActivity.game;
+
 public class OnSpawnSetHealth extends ActionEffectInternal {
     @Override
     public String Execute(Section[][] ship, ThreatInternal threat)
     {
-        threat.health = MainActivity.game.missileCount;
+        threat.health = game.missileCount;
+        if(threat.health == 0)
+        {
+            game.deadThreats.add(threat);
+        }
         return "";
     }
 }
